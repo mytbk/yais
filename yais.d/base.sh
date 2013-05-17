96d24ce78b48bdfdda534134196147c33f0852b7
Iru Cai <mytbk920423@gmail.com>
2013-05-17 21:51:34 +0800

    yais functions changed

--- yais.d/base.sh
+++ yais.d/base.sh
@@ -1,24 +1,12 @@
 editor(){
-	echo -n "Your favorate editor(nano,vi,...):"
-	read EDITOR
-	EDITOR=${EDITOR:-"vi"}
+	EDITOR=$(dialog --stdout --inputbox "Your favorite editor" 0 0 vi)
 	export EDITOR
-}
-
-mirrorselect(){
-	$EDITOR /etc/pacman.d/mirrorlist
 }
 
 testnet(){
 	pacman -Sy && echo -e "\033[32mNetwork OK" || echo -e "\033[31mNetwork failed."
 	echo -e "\033[0mPress enter to continue"
 	read
-}
-
-install_pkg(){
-	[ -f /tmp/packages ] || cp /etc/yais.d/packages.example /tmp/packages
-	$EDITOR /tmp/packages
-	pacstrap ${ROOTDIR} $(sed -e 's/^[ \t]*//g' -e '/^#/d' /tmp/packages)
 }
 
 install_grub(){
